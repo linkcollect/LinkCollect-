26165b82318ee1b20583606263791bd26ef50b05
Harsh8055 <harshsingh.eth@gmail.com>
2023-09-12 00:56:46 +0530

    test log

--- src/controllers/analyticsController.ts
+++ src/controllers/analyticsController.ts
@@ -1,6 +1,10 @@
 import { analytics } from "googleapis/build/src/apis/analytics";
 import { Collection, User, Timeline, SearchHistory } from "../models";
 import { liveMessage } from "../constants/liveMessage";
+import EmailService from "../services/emailService";
+
+const emailService = new EmailService();
+
 const getAll = async (req, res) => {
   try {
     const collections = await Collection.find();
@@ -31,7 +35,7 @@ const getAll = async (req, res) => {
 
 const getLiveMessage = async (req, res) => {
   try {
-    
+    console.log("hostname", req.hostname)
     return res.status(201).json({
       data: liveMessage,
       success: true,
@@ -66,6 +70,32 @@ const getSearchHistory = async (req, res) => {
   }
 };
 
-const analyticsController = { getAll, getLiveMessage, getSearchHistory };
+
+const sendWeekyEmail = async (req, res) => {
+  try {
+    
+    const response = await emailService.sendWeeklyEmail();
+    
+    return res.status(201).json({
+      data: response,
+      success: true,
+      message: "Successfully sent emails",
+      err: {},
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to get the analytics",
+      err: error,
+    });
+  }
+};
+
+
+
+
+const analyticsController = { getAll, getLiveMessage, getSearchHistory, sendWeekyEmail };
 
 export default analyticsController;
