0186dd1eb07684442af83a597c9a1af53f3626c2
Harsh8055 <harshsingh.eth@gmail.com>
2023-10-10 16:13:34 +0530

    fix nginx and certbot issues

--- images/cert-init.sh
+++ images/cert-init.sh
@@ -78,3 +78,6 @@ echo
 
 echo "### Reloading nginx ..."
 docker-compose -f docker-compose.backend.yml exec nginx nginx -s reload
+
+
+# certbot certonly --webroot -w /var/www/certbot --staging -d backup.linkcollect.io --email harshsingh.eth@gmail.com --rsa-key-size 4096 --agree-tos --force-renewal
